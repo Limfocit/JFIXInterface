d284743d12ce6f63de10c103f59d408b73546588
Limfocit <valentin.michajlenko@gmail.com>
2014-01-07 22:59:49 +0400

    FIX 4.2 data request

--- src/main/java/com/limfocit/jfixinterface/JFIXApplication.java
+++ src/main/java/com/limfocit/jfixinterface/JFIXApplication.java
@@ -1,18 +1,30 @@
 package com.limfocit.jfixinterface;
 
+import java.util.UUID;
+
 import quickfix.Application;
 import quickfix.DoNotSend;
 import quickfix.FieldNotFound;
 import quickfix.IncorrectDataFormat;
 import quickfix.IncorrectTagValue;
 import quickfix.Message;
-import quickfix.fix44.MessageCracker;
+import quickfix.Session;
+import quickfix.fix42.MarketDataIncrementalRefresh;
+import quickfix.fix42.MarketDataRequest;
+import quickfix.fix42.MarketDataRequestReject;
+import quickfix.fix42.MarketDataSnapshotFullRefresh;
+import quickfix.fix42.MessageCracker;
 import quickfix.RejectLogon;
 import quickfix.SessionID;
 import quickfix.UnsupportedMessageType;
-import quickfix.field.ClOrdID;
+import quickfix.field.MDEntryType;
+import quickfix.field.MDReqID;
+import quickfix.field.MDUpdateType;
+import quickfix.field.MarketDepth;
 import quickfix.field.MsgType;
 import quickfix.field.ResetSeqNumFlag;
+import quickfix.field.SubscriptionRequestType;
+import quickfix.field.Symbol;
 import quickfix.field.Username;
 import quickfix.field.Password;
 
@@ -22,19 +34,36 @@ public class JFIXApplication extends MessageCracker implements Application {
 	private final String Password;
 	
 	private SessionID SessionId;
+	private Session MySession;
 	
 	public JFIXApplication(String username, String password) {
 		super();		
 		Username = username;
 		Password = password;
 	}
+	
+	public boolean subscribeData() {
+		if (MySession == null) return false;
+		MarketDataRequest mes = new MarketDataRequest(
+				new MDReqID(UUID.randomUUID().toString()), 
+				new SubscriptionRequestType(SubscriptionRequestType.SNAPSHOT_PLUS_UPDATES), 
+				new MarketDepth(1));
+		mes.set(new MDUpdateType(MDUpdateType.INCREMENTAL_REFRESH));
+		MarketDataRequest.NoRelatedSym relatedSymbols = new MarketDataRequest.NoRelatedSym();
+		relatedSymbols.set(new Symbol("EUR/USD"));
+		mes.addGroup(relatedSymbols);
+		MarketDataRequest.NoMDEntryTypes entryTypes = new MarketDataRequest.NoMDEntryTypes();
+		entryTypes.set(new MDEntryType(MDEntryType.TRADE));
+		mes.addGroup(entryTypes);
+		MySession.send(mes);
+		return true;
+	}
 
 	@Override
 	public void fromAdmin(Message arg0, SessionID arg1) throws FieldNotFound,
 			IncorrectDataFormat, IncorrectTagValue, RejectLogon {
-		// TODO Auto-generated method stub
-		
-	}
+		// TODO Auto-generated method stub		
+	}	
 
 	@Override
 	public void fromApp(final Message message, final SessionID sessionId) throws FieldNotFound,
@@ -43,14 +72,14 @@ public class JFIXApplication extends MessageCracker implements Application {
 	}
 
 	@Override
-	public void onCreate(SessionID arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onCreate(SessionID sessionID) {
+		 Session.lookupSession(sessionID).getLog().onEvent("Valid order types: ");
 	}
 
 	@Override
 	public void onLogon(final SessionID sessionId) {
 		SessionId = sessionId;
+		MySession = Session.lookupSession(sessionId);
 	}
 
 	@Override
@@ -80,11 +109,18 @@ public class JFIXApplication extends MessageCracker implements Application {
 	
 	public SessionID getSessionID() {
 		return SessionId;
+	}	
+	
+	public void onMessage(MarketDataIncrementalRefresh message, SessionID sessionID) {
+		System.out.println("increment");
 	}
 	
-	public void onMessage( quickfix.fix44.NewOrderSingle message, SessionID sessionID )
-		      throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
-		ClOrdID clOrdID = new ClOrdID();
-		message.get(clOrdID);
+	public void onMessage(MarketDataSnapshotFullRefresh message, SessionID sessionID) {
+		System.out.println("full");
+	}
+	
+	public void onMessage(MarketDataRequestReject message, SessionID sessionID) {
+		System.out.println("reject");
 	}
 }
+
--- src/main/java/com/limfocit/jfixinterface/main/Main.java
+++ src/main/java/com/limfocit/jfixinterface/main/Main.java
@@ -2,19 +2,21 @@ package com.limfocit.jfixinterface.main;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.limfocit.jfixinterface.JFIXApplication;
 
 import quickfix.Acceptor;
 import quickfix.Application;
 import quickfix.ConfigError;
+import quickfix.DefaultMessageFactory;
 import quickfix.FileLogFactory;
 import quickfix.FileStoreFactory;
 import quickfix.LogFactory;
 import quickfix.MessageStoreFactory;
 import quickfix.SessionSettings;
 import quickfix.SocketAcceptor;
-import quickfix.fix44.MessageFactory;
+import quickfix.MessageFactory;
 
 
 public class Main {
@@ -23,14 +25,21 @@ public class Main {
 		try {
 		    String fileName = "src\\main\\resources\\settings.ini";
 	
-		    Application application = new JFIXApplication("", "");
+		    JFIXApplication application = new JFIXApplication("fxddi23728", "fxdd1234");
 	
 		    SessionSettings settings = new SessionSettings(new FileInputStream(fileName));
 		    MessageStoreFactory storeFactory = new FileStoreFactory(settings);
 		    LogFactory logFactory = new FileLogFactory(settings);
-		    MessageFactory messageFactory = new MessageFactory();
+		    MessageFactory messageFactory = new DefaultMessageFactory();
 		    Acceptor acceptor = new SocketAcceptor(application, storeFactory, settings, logFactory, messageFactory);
 		    acceptor.start();
+		    while (application.subscribeData()) {}
+		    System.out.println("sub data");
+		    try {
+				System.in.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		    acceptor.stop();
 		} catch (FileNotFoundException | ConfigError e) {
 			e.printStackTrace();
